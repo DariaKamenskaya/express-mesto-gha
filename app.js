c44ea0c385ff890f48c5e63ce77f4fd1349ceb1d
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2022-02-18 20:14:30 +0300

    feat: создать express-сервер и настроить хот релоуд

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.get('/', (req,res) => {
+  res.send('Hello !')
+});
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`)
+})
