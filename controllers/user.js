d26297afb86a20a0a1c72a7033ef086a0aa2029a
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2022-02-24 22:51:17 +0300

    hotfix: добавить обработку ошибок для роута users

--- controllers/user.js
+++ controllers/user.js
@@ -1,64 +1,90 @@
 const user = require("../models/user");
 
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await user.find({});
+    res.status(200).send(users);
+  }
+  catch (err) {
+    res.status(500).send({ message: 'Произошла ошибка!', ...err });
+  }
+};
 
-exports.getUsers = async (req,res) => {
-  const users = await user.find({});
-  res.status(200).send(users);
-}
-
-exports.getUserbyId = async (req,res) => {
+exports.getUserbyId = async (req, res) => {
   try {
     const userSpec = await user.findById(req.params.userId);
     if (userSpec) {
-      res.status(200).send({data: userSpec});
+      res.status(200).send({ data: userSpec });
     } else {
-      res.status(404).send({message: 'Пользователь не найден'});
+      res.status(404).send({ message: 'Пользователь не найден' });
     }
   }
-  catch(err) {
-    res.status(500).send({message: 'Произошла ошибка!', ...err});
+  catch (err) {
+    if (err.name === 'CastError') {
+      res.status(400).send({ message: 'Невалидный id ' });
+    }
+    res.status(500).send({ message: 'Произошла ошибка!', ...err });
   }
-}
+};
 
-exports.createUser = async (req,res) => {
+exports.createUser = async (req, res) => {
   try {
     const { name, about, avatar } = req.body;
     user.create({ name, about, avatar });
     res.status(201).send({ data: user });
   }
-  catch(err) {
-    res.status(500).send({message: 'Произошла ошибка!', ...err});
+  catch (err) {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({ message: 'Некорректные данные' });
+    }
+    res.status(500).send({ message: 'Произошла ошибка!', ...err });
   }
-}
+};
 
-exports.patchUserMe = async (req,res) => {
+exports.patchUserMe = async (req, res) => {
   try {
     const { name, about } = req.body;
-    const ownerId = req.user._id;
-    const userPatchMe = await user.findByIdAndUpdate(ownerId, {name, about} );
-    if (userPatchMe) {
-      res.status(200).send({data: userPatchMe});
+    const opts = { new: true, runValidators: true };
+    if (!name || !about) {
+      res.status(400).send({ message: 'Поля "name" и "about" должно быть заполнены' });
     } else {
-      res.status(400).send({message: 'Переданы некорректные данные'});
+      const ownerId = req.user._id;
+      const userPatchMe = await user.findByIdAndUpdate(ownerId, { name, about }, opts);
+      if (userPatchMe) {
+        res.status(200).send({ data: userPatchMe });
+      } else {
+        res.status(404).send({ message: 'Переданы некорректные данные' });
+      }
     }
   }
-  catch(err) {
-    res.status(500).send({message: 'Произошла ошибка!', ...err});
+  catch (err) {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({ message: 'Некорректные данные' });
+    }
+    res.status(500).send({ message: 'Произошла ошибка!', ...err });
   }
-}
+};
 
-exports.patchUserAvatar = async (req,res) => {
+exports.patchUserAvatar = async (req, res) => {
   try {
-    const { avatar } = req.body;
-    const ownerId = req.user._id;
-    const userPatchAvatar = await user.findByIdAndUpdate(ownerId, {avatar});
-    if (userPatchAvatar) {
-      res.status(200).send({data: userPatchAvatar});
+    if (!req.body.avatar) {
+      res.status(400).send({ message: 'Поле "avatar" должно быть заполнено' });
     } else {
-      res.status(400).send({message: 'Переданы некорректные данные'});
+      const { avatar } = req.body;
+      const ownerId = req.user._id;
+      const opts = { new: true, runValidators: true };
+      const userPatchAvatar = await user.findByIdAndUpdate(ownerId, { avatar }, opts);
+      if (userPatchAvatar) {
+        res.status(200).send({ data: userPatchAvatar });
+      } else {
+        res.status(404).send({ message: 'Переданы некорректные данные' });
+      }
     }
   }
-  catch(err) {
-    res.status(500).send({message: 'Произошла ошибка!', ...err});
+  catch (err) {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({ message: 'Некорректные данные' });
+    }
+    res.status(500).send({ message: 'Произошла ошибка!', ...err });
   }
-}
+};
