764beda9ced1ef15f7cc9bbebd583afea3302c72
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2022-03-06 14:31:06 +0300

    feat: сделать поля name, about, avatar не обязательными и добавить дефолтные значения в моделе схемы user

--- models/user.js
+++ models/user.js
@@ -4,19 +4,19 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
-    required: true,
+    default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
